d1724c47790a3556f13b92d241f625c05545ea87
SUJUN <393834461@qq.com>
2020-11-03 17:56:43 +0800

    saas

--- src/main/java/com/dna/EsapiApplication.java
+++ src/main/java/com/dna/EsapiApplication.java
@@ -9,6 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EsapiApplication {
 
     public static void main(String[] args) {
+
+        /*启动类指定项目中使用的日志框架*/
+        /*解决
+        log4j:WARN No appenders could be found for logger (org.apache.dubbo.common.logger.LoggerFactory).
+        log4j:WARN Please initialize the log4j system properly.*/
+        System.setProperty("dubbo.application.logger", "log4j2");
+
         SpringApplication.run(EsapiApplication.class, args);
     }
 
